819790786c5157398efcda7eade431fc1b53b715
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-02 21:13:04 +0530

    push function

--- Stacks.c
+++ Stacks.c
@@ -5,3 +5,12 @@ struct stack {
     int s[size];
     int top;
 } st;
+
+void push(int item) {
+    if (!isfull()) {
+        st.top++;
+        st.s[st.top] = item;
+    } else {
+        printf("Stack is full. Cannot push item.\n");
+    }
+}
