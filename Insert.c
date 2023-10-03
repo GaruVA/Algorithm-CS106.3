093308f8a361fd2cd81ed46cb850dd158538edf9
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-03 21:05:54 +0530

    removed size as it was unnecessary

--- Insert.c
+++ Insert.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void insert(int array[], int size, int rightIndex, int value) {
+void insert(int array[], int rightIndex, int value) {
     int i;
     for(i = rightIndex; i >= 0 && array[i] > value; i--) {
         array[i+1] = array[i];
@@ -11,13 +11,7 @@ void insert(int array[], int size, int rightIndex, int value) {
 
 int main() {
     int array[] = {3, 5, 7, 11, 13, 2, 9, 6};
-    int size = 8;
-    insert(array, size, 4, 2);
-    insert(array, size, 5, 9);
-    insert(array, size, 6, 6);
-    printf("Sorted Array: ");
-    for(int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
+    insert(array, 4, 2);
+    insert(array, 5, 9);
+    insert(array, 6, 6);
 }
