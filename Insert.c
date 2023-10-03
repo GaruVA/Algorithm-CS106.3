3954abdd8eb1874b276dc329ee6da9656bf1a3a8
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-03 20:42:42 +0530

    main function

--- Insert.c
+++ Insert.c
@@ -6,5 +6,14 @@ void insert(int array[], int size, int rightIndex, int value) {
 
 
 int main() {
-
+    int array[] = {3, 5, 7, 11, 13, 2, 9, 6};
+    int size = 8;
+    insert(array, size, 4, 2);
+    insert(array, size, 5, 9);
+    insert(array, size, 6, 6);
+    printf("Sorted Array: ");
+    for(int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
