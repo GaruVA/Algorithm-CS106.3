73cd35ae254f82a62220542a7814348c61a4237c
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-04 20:33:30 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -5,7 +5,18 @@ void bubbleSort(int array[], int size) {
     bool swap;
     int temp;
     for(int i = 0; i < size - 1; i++) {
-        for()
+        swap = false;
+        for(int j = 0; j < size - i - 1; i++) {
+            if(array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swap = true;
+            }
+        }
+        if(!swap) {
+            break;
+        }
     }
 }
 
