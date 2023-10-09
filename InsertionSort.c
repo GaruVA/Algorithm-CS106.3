2c56a28e208706749684264c01332b328966db2b
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-09 21:54:04 +0530

    Updated insert function

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 void insert(int array[], int rightIndex, int value) {
-
+    int i;
+    for(i = rightIndex; i >= 0 && array[i] > value; i--) {
+        array[i+1] = array[i];
+    }
+    array[i+1] = value;
 }
 
 void insertionSort(int array[], int size) {
