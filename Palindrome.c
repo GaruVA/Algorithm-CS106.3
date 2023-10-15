d0f6b45a6186a11a9083b36e92f4b384e53aa43a
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-15 21:42:51 +0530

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,32 @@
+// Returns the first character of the string str
+var firstCharacter = function(str) {
+    return str.slice(0, 1);
+};
+
+// Returns the last character of a string str
+var lastCharacter = function(str) {
+    return str.slice(-1);
+};
+
+// Returns the string that results from removing the first
+//  and last characters from str
+var middleCharacters = function(str) {
+    return str.slice(1, -1);
+};
+
+var isPalindrome = function(str, size) {
+    if (str.length <= 1) {
+        return true;
+    }
+
+    if(firstCharacter(str) !== lastCharacter(str)) {
+        return false;
+    }
+
+    return isPalindrome(middleCharacters(str));
+};
+
+var checkPalindrome = function(str) {
+    println("Is this word a palindrome? " + str);
+    println(isPalindrome(str));
+};
