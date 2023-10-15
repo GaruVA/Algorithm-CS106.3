bdb56bf01ddcad4853881fb850d572556c0ad526
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-15 10:19:16 +0530

    Update RecursiveFactorial.c

--- RecursiveFactorial.c
+++ RecursiveFactorial.c
@@ -6,4 +6,8 @@ int factorial(int n) {
 }
 
 int main() {
+    printf("The value of 5! should be %d\n", 5*4*3*2*1);
+    printf("The value of 5! is %d\n",factorial(5));
+    printf("The value of 0! should be 1\n");
+    printf("The value of 0! is %d\n", factorial(0));
 }
