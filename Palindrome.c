c8300afe460d4afebda7469f4f7e36280f3b718c
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-17 12:50:40 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int isPalindrome(char *str,int front, int rear) {
+int isPalindrome(char *str, int front, int rear) {
+    if (front >= rear) {
+        return 1;
+    }
+
+    if (str[front] != str[rear]) {
+        return 0;
+    }
+
+    return isPalindrome(str, front + 1, rear - 1);
 }
 
 int main() {
