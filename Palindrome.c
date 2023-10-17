d58cafd2546983fc7d03759ecac6f8d8ad3785c5
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-17 12:49:52 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 
 int isPalindrome(char *str,int front, int rear) {
-    if (front == rear) {
-        return 1;
-    }
-
-    if(str[front] != str[rear]) {
-        return 0;
-    }
-
-    return isPalindrome(str, front + 1, rear - 1);
-};
+}
 
 int main() {
-    char word[] = "rator";
-    printf("%d\n",isPalindrome(word, 0, 4));
 }
