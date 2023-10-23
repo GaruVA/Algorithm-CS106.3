d1f93bb7b4fe244c6e1b65147a1b9369db1cbc22
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-23 20:25:19 +0530

    Update RecursivePowers.c

--- RecursivePowers.c
+++ RecursivePowers.c
@@ -8,3 +8,22 @@ int isOdd(int n) {
     return !isEven(n);
 }
 
+int power(int n, int x) {
+    if(n == 0) {
+        return 1;
+    }
+    if(n < 0) {
+        return 1/power(x, -n);
+    }
+    if(isOdd(n)) {
+        return x * power(x, n - 1);
+    }
+    if(isEven(n)) {
+        int halfpower = power(x, n / 2);
+        return halfpower * halfpower;
+    }
+}
+
+void displayPower(int n, int x) {
+    printf("%d^%d = %d\n", n, x, power(n, x));
+}
