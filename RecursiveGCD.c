874cf18b4187ab48aee3bb6546cb450aaa9c725b
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-11-03 21:48:05 +0530

    Update RecursiveGCD.c

--- RecursiveGCD.c
+++ RecursiveGCD.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int calculateGCD(int a, int b) {
+    int temp;
+
+    // Ensure non-negative values for a and b
+    a = abs(a);
+    b = abs(b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
