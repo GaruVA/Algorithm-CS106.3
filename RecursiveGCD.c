7e08440e7e192fbff10f3486bbf03461c026b0ee
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-11-03 21:48:55 +0530

    Update RecursiveGCD.c

--- RecursiveGCD.c
+++ RecursiveGCD.c
@@ -16,3 +16,21 @@ int calculateGCD(int a, int b) {
 
     return a;
 }
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+
+    int gcd = calculateGCD(num1, num2);
+
+    printf("The GCD of %d and %d is %d\n", num1, num2, gcd);
+
+    return 0;
+}
+
+
